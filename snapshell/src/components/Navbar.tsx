278ff4d5ae5e7af7f77f8948d80773551e0951f9
BozinovskiDaniel <BozinovskiDaniel@hotmail.com>
2024-06-18 13:44:25 +1000

    Add kinde integration for handling login/logout functionality

--- snapshell/src/components/Navbar.tsx
+++ snapshell/src/components/Navbar.tsx
@@ -3,10 +3,16 @@ import MaxWidthWrapper from './MaxWidthWrapper';
 import Link from 'next/link';
 import { buttonVariants } from './ui/button';
 import { ArrowRight } from 'lucide-react';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const Navbar = () => {
-  const isAdmin = false;
-  const user = undefined;
+// Add async to the navbar here to make it a react server component
+// So it runs on the server
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
